c76fc86a282ad1884c8f1dc6fb3bff6572c5ffa7
DenerRod <denerrodriguesbarrientos@gmail.com>
2022-05-18 20:18:35 -0300

    Exercicios de fixaçao 11.1

--- bloco-11-Componentes-com-estado-Eventos-e-formulario/aula11.1-componentes-com-estado-e-eventos/introducao-aos-eventos/fancy-buttons/src/App.js
+++ bloco-11-Componentes-com-estado-Eventos-e-formulario/aula11.1-componentes-com-estado-e-eventos/introducao-aos-eventos/fancy-buttons/src/App.js
@@ -1,25 +1,77 @@
 import React from 'react';
 import './App.css';
 
-function handleClick1() {
-  console.log('Click Feito!');
-};
-
-function handleClick2() {
-  console.log('Click Feito Mais uma vez!');
-};
-
-function handleClick3() {
-  console.log('Click Feito última vez!');
-};
 
 class App extends React.Component {
+/*   constructor() {    -------------> (Como é construido o contructor de uma classe)
+
+    super() --------------> (Super serve para extender a função constructor que já é criada e aplicada nativamente (com nossa lógica incrementada) no react na construção de uma class, através do 'extends')
+
+    // A função abaixo vincula "manualmente" o `this` à nossa função
+    this.handleClick = this.handleClick.bind(this)
+  } */
+  constructor() {
+    super()
+    this.state = {
+      numerosDeClick1: 0,
+      numerosDeClick2: 0,
+      numerosDeClick3: 0,
+    }
+    this.handleClick1 = this.handleClick1.bind(this)
+    this.handleClick2 = this.handleClick2.bind(this)
+    this.handleClick3 = this.handleClick3.bind(this)
+  }
+  
+
+  handleClick1() { // Função com arrow function '=>' não é necessário fazer o bind no constructor
+    this.setState(({ numerosDeClick1 }) => ({
+      numerosDeClick1: numerosDeClick1 + 1,        
+    }))
+
+  };
+
+  handleClick2() { // Função com arrow function '=>' não é necessário fazer o bind no constructor
+    this.setState(({ numerosDeClick2 }) => ({
+      numerosDeClick2: numerosDeClick2 + 1,         
+    }))
+
+  };
+
+  handleClick3() { // Função com arrow function '=>' não é necessário fazer o bind no constructor
+    this.setState(({ numerosDeClick3 }) => ({
+      numerosDeClick3: numerosDeClick3 + 1,  
+    }))
+
+  };
+
+  putColor(num) {
+   return num % 2 === 0 ? 'green' : 'white';
+  };
+  
+  
   render() {
+    const { numerosDeClick1, numerosDeClick2, numerosDeClick3 } = this.state;
     return (
       <div>
-        <button onClick={handleClick1}>Meu button</button>
-        <button onClick={handleClick2}>Meu button</button>
-        <button onClick={handleClick3}>Meu button</button>
+
+        <button 
+        onClick={this.handleClick1}
+        style={{backgroundColor: this.putColor(numerosDeClick1)}}>
+          Botão 1 | Count = { numerosDeClick1 }
+        </button>
+
+        <button 
+        onClick={this.handleClick2}
+        style={{backgroundColor: this.putColor(numerosDeClick2)}}>
+           Botão 1 | Count = { numerosDeClick2 }
+        </button>
+
+        <button 
+        onClick={this.handleClick3}
+        style={{backgroundColor: this.putColor(numerosDeClick3)}}>
+           Botão 1 | Count = { numerosDeClick3 }
+        </button>
+
       </div>
     );
   }
